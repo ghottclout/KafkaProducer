338dc658409b6d5df6ed5faba55cbfecc19172e5
Guilherme Hott <guilherme@clout.com>
2017-05-03 19:13:13 -0700

    Simple Kafka Producer

---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+
+namespace KafkaProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new KafkaOptions
+               (new Uri("http://localhost:9092")
+                );
+            var router = new BrokerRouter(options);
+
+            var client = new KafkaNet.Producer(router);
+
+            for(int i = 0; i < 10; i++)
+            {
+                client.SendMessageAsync("TestTopic", new[]
+                   { new Message(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss:fff") + " | Producer Console | " + i) }).Wait();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
